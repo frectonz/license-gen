b787415120e37d4ba6573104b26354ed328aa19d
frectonz <fraol0912@gmail.com>
2023-10-28 20:13:37 +0300

    feat: add `Unlicense` license

--- src/licenses.rs
+++ src/licenses.rs
@@ -7,6 +7,7 @@ const APACHE_V2: &str = include_str!("licenses/Apache-2.0.txt");
 const BSD_3_CLAUSE: &str = include_str!("licenses/BSD-3-Clause.txt");
 const BSD_2_CLAUSE: &str = include_str!("licenses/BSD-2-Clause.txt");
 const BSD_1_CLAUSE: &str = include_str!("licenses/BSD-1-Clause.txt");
+const UNLICENSE: &str = include_str!("licenses/UNLICENSE.txt");
 
 pub fn get_license_content() -> Result<String> {
     let licenses = vec![
@@ -16,6 +17,7 @@ pub fn get_license_content() -> Result<String> {
         "BSD 3-Clause",
         "BSD 2-Clause",
         "BSD 1-Clause",
+        "Unlicense",
     ];
     let license = Select::new("Choose your license?", licenses).prompt()?;
     let content = match license {
@@ -25,6 +27,7 @@ pub fn get_license_content() -> Result<String> {
         "BSD 3-Clause" => BSD_3_CLAUSE.to_owned(),
         "BSD 2-Clause" => BSD_2_CLAUSE.to_owned(),
         "BSD 1-Clause" => BSD_1_CLAUSE.to_owned(),
+        "Unlicense" => UNLICENSE.to_owned(),
         _ => return Err(anyhow!("Unknown license")),
     };
 
