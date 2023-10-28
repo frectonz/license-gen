98632ed6120d02c2e8981b50f296ef0d0540bebb
frectonz <fraol0912@gmail.com>
2023-10-28 19:56:32 +0300

    feat: add `BSD 3-Clause` license

--- src/licenses.rs
+++ src/licenses.rs
@@ -4,14 +4,16 @@ use inquire::Select;
 const MIT: &str = include_str!("licenses/MIT.txt");
 const ISC: &str = include_str!("licenses/ISC.txt");
 const APACHE_V2: &str = include_str!("licenses/Apache-2.0.txt");
+const BSD_3_CLAUSE: &str = include_str!("licenses/BSD-3-Clause.txt");
 
 pub fn get_license_content() -> Result<String> {
-    let licenses = vec!["MIT", "ISC", "Apache-2.0"];
+    let licenses = vec!["MIT", "ISC", "Apache 2.0", "BSD 3-Clause"];
     let license = Select::new("Choose your license?", licenses).prompt()?;
     let content = match license {
         "MIT" => MIT.to_owned(),
         "ISC" => ISC.to_owned(),
-        "Apache-2.0" => APACHE_V2.to_owned(),
+        "Apache 2.0" => APACHE_V2.to_owned(),
+        "BSD 3-Clause" => BSD_3_CLAUSE.to_owned(),
         _ => return Err(anyhow!("Unknown license")),
     };
 
