2718f0fb2e8bb60fefb698442ba28f4560e30f28
frectonz <fraol0912@gmail.com>
2023-10-28 20:06:35 +0300

    feat: add `BSD 1-Clause` license

--- src/licenses.rs
+++ src/licenses.rs
@@ -6,9 +6,17 @@ const ISC: &str = include_str!("licenses/ISC.txt");
 const APACHE_V2: &str = include_str!("licenses/Apache-2.0.txt");
 const BSD_3_CLAUSE: &str = include_str!("licenses/BSD-3-Clause.txt");
 const BSD_2_CLAUSE: &str = include_str!("licenses/BSD-2-Clause.txt");
+const BSD_1_CLAUSE: &str = include_str!("licenses/BSD-1-Clause.txt");
 
 pub fn get_license_content() -> Result<String> {
-    let licenses = vec!["MIT", "ISC", "Apache 2.0", "BSD 3-Clause", "BSD 2-Clause"];
+    let licenses = vec![
+        "MIT",
+        "ISC",
+        "Apache 2.0",
+        "BSD 3-Clause",
+        "BSD 2-Clause",
+        "BSD 1-Clause",
+    ];
     let license = Select::new("Choose your license?", licenses).prompt()?;
     let content = match license {
         "MIT" => MIT.to_owned(),
@@ -16,6 +24,7 @@ pub fn get_license_content() -> Result<String> {
         "Apache 2.0" => APACHE_V2.to_owned(),
         "BSD 3-Clause" => BSD_3_CLAUSE.to_owned(),
         "BSD 2-Clause" => BSD_2_CLAUSE.to_owned(),
+        "BSD 1-Clause" => BSD_1_CLAUSE.to_owned(),
         _ => return Err(anyhow!("Unknown license")),
     };
 
